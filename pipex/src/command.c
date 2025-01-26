82dfdb9aecb1c6407ac7b19ea131d7ec494753f0
Ivan Petrov <ivanpetrov.main@gmail.com>
2025-01-26 17:25:47 +0700

    fix: free() cntx when cmd is invalid

--- pipex/src/command.c
+++ pipex/src/command.c
@@ -6,7 +6,7 @@
 /*   By: ipetrov <ipetrov@student.42bangkok.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/12 19:04:47 by ipetrov           #+#    #+#             */
-/*   Updated: 2025/01/26 16:21:57 by ipetrov          ###   ########.fr       */
+/*   Updated: 2025/01/26 17:24:54 by ipetrov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ int run_cmd(t_cntx *cntx, char *cmd)
 	if (execve(pathname, argv, cntx->envp) == ERROR)
 	{
 		ft_parrclean(0, free, argv, NULL);
+		ft_parrclean(0, free, cntx->envp, NULL);
+		free(cntx);
 		return (error(EXECVE, NULL));
 	}
 	return (SUCCESS);
